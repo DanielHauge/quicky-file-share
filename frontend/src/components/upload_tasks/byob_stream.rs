4cd7d247a2a17454320beefbf1620c6fdef704d7
Daniel Hauge <animcuil@gmail.com>
2023-08-01 19:43:08 +0200

    Progress on async body eval!

--- frontend/src/components/upload_tasks/byob_stream.rs
+++ frontend/src/components/upload_tasks/byob_stream.rs
@@ -1,16 +1,26 @@
 use std::{error::Error, str::Bytes};
-
+use async_stream::stream;
+use js_sys::Uint8Array;
+use serde::{Serialize, Deserialize};
 use wasm_bindgen::JsValue;
+use wasm_bindgen_futures::JsFuture;
 use web_sys::{ReadableStreamByobReader, File};
 use futures_core::{stream::Stream, stream::TryStream};
+
+// use crate::home::upload_input::drop_zone::upload::ReadResult;
 
 #[derive(Debug)]
 pub struct ByobStream {
     reader: ReadableStreamByobReader,
     done: bool,
+    buffer: Uint8Array,
 }
 
-
+#[derive(Debug, Serialize, Deserialize)]
+struct ReadResult {
+    done: bool,
+    value: Vec<u8>,
+}
 
 // futures_core::stream::TryStream + Send + Sync + 'static
 
@@ -22,19 +32,37 @@ impl ByobStream {
             Ok(reader) => Ok(ByobStream {
                 reader,
                 done: false,
+                buffer: js_sys::Uint8Array::new(&JsValue::from(4000))
             }),
             Err(e) => Err(e),
         }
     }
 }
 
-impl Stream for ByobStream {
-    type Item = Result<String, String>;
 
-    fn poll_next(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Option<Self::Item>> {
-        todo!()
+pub fn to_stream(bs: ByobStream) -> impl Stream<Item = Result<Vec<u8>, String>> {
+    stream! {
+        let mut done = false;
+        while !done {
+            let future = JsFuture::from(bs.reader.read_with_array_buffer_view(&bs.buffer)).await;
+            // match gg
+            match future {
+                Ok(gg) => {
+                    let res: ReadResult = serde_wasm_bindgen::from_value(gg).expect("failed to parse readable stream read result");
+                    yield Ok(res.value);
+                    done = res.done;
+                },
+                Err(e) => {
+                    // console.error!
+                    done = true;
+                }
+            }
+
+        }
     }
-}
+} 
+
+
 
 
 
--- frontend/src/components/upload_tasks/upload.rs
+++ frontend/src/components/upload_tasks/upload.rs
@@ -1,4 +1,5 @@
 
+use futures_util::StreamExt;
 use js_sys::{Boolean, Uint8Array, Error};
 use reqwest::Request;
 use wasm_bindgen::JsValue;
@@ -26,17 +27,18 @@ pub async fn upload_file(file : File) -> Result<String, Error> {
     let reader = ReadableStreamByobReader::new(&stream).ok().unwrap();
     
     let mut done = true;
+    let mut bytes = Vec::new();
     while done {
         let gg = JsFuture::from(reader.read_with_array_buffer_view(&js_sys::Uint8Array::new(&JsValue::from(4000)))).await?;
         let res: ReadResult = serde_wasm_bindgen::from_value(gg).expect("failed to parse readable stream read result");
         console::log_1(&res.value.len().into());
+        bytes.extend_from_slice(&res.value);
+
         done = !res.done;
     }
 
-    let str = byob_stream::ByobStream::from_file(file).ok().unwrap();
     
-    
-    let yo = reqwest::Body::wrap_stream(str);
+    let yo = reqwest::Body::from(bytes);
     
     return Ok(String::from("file uploaded."))
 
